ac813232996ba0661544b9f2dbdce2d378d5a1af
Mario-Fernando24 <3115408069.mafe@gmail.com>
2023-06-01 11:42:23 -0500

    agregar menu

--- app/src/main/java/com/example/easyfood/MainActivity.kt
+++ app/src/main/java/com/example/easyfood/MainActivity.kt
@@ -2,10 +2,28 @@ package com.example.easyfood
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val bottomNavigation = findViewById<BottomNavigationView>(R.id.btn_nav)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.host_fragment) as NavHostFragment
+        navController = navHostFragment.navController
+
+        val myNavUIComponent: View // Aquí debes proporcionar tu componente de interfaz de usuario
+        NavigationUI.setupWithNavController(bottomNavigation, navController)
+
     }
 }
