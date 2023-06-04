5a1e16c29ef4b9ecc099b1dc3d836754c357ef93
Mario-Fernando24 <3115408069.mafe@gmail.com>
2023-06-04 17:42:37 -0500

    adapter comidas popular

---
+++ app/src/main/java/com/example/easyfood/adapters/MostPopularAdapter.kt
@@ -0,0 +1,22 @@
+package com.example.easyfood.adapters
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.easyfood.databinding.PopularItemBinding
+
+class MostPopularAdapter(): RecyclerView.Adapter<MostPopularAdapter.PopularMealViewHolder>() {
+
+    class PopularMealViewHolder(private val binding: PopularItemBinding): RecyclerView.ViewHolder(binding.root)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PopularMealViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: PopularMealViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+}
